ffa7e86f9697d1cd8baf01fc2bddf2a97154a45b
Hammad-Malik1 <hammadmalikhm2018@gmail.com>
2026-07-14 23:32:04 +0100

    MySQL database connection - Source: AI

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
 from flask import Flask                             # import class
-from flask import render_template                   # render & return html file
+from flask import render_template
+from flask_mysqldb import MySQL                   # render & return html file
 
-app = Flask(__name__)                               # instance of class
+app = Flask(__name__)     
 
 
-@app.route("/")                                     # root U
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = ""
+app.config["MYSQL_DB"] = "issue_tracker"
+
+
+mysql = MySQL(app)                          # instance of class
+
+
+@app.route("/")                                     # root URL
 def index():
     return render_template("index.html")
 
 
-if __name__ == "__main__":
+@app.route("/test-db")                              # DB connection
+def test_db():
+    try:
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT DATABASE();")
+        result = cursor.fetchone()
+        cursor.close()
+
+        return f"Connected Successfully! Current Database: {result[0]}"
+
+    except Exception as e:
+        return f"Database Connection Failed: {e}"
+
+
+if __name__ == "__main__":                          #directly run
     app.run(debug=True)
